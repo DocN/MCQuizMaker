13f75ef307744bf498b84cb64dc1c103e5abcb1e
DrN <031247ryan@gmail.com>
2018-10-02 11:08:52 -0700

    added first commit of quiz demo

---
+++ js/Model/Word.js
@@ -0,0 +1,17 @@
+class Word {
+    constructor(word, desc) {
+      this.word = word;
+      this.description = desc;
+    }
+
+
+
+    set setDesc(desc) {
+        this.description = desc;
+    }
+
+    get getDesc() {
+        return this.description;
+    }
+}
+  
