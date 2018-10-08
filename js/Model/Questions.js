bcfb3cfa1eaf4ad87ca122137d26fafd862d3a21
DocN <031247ryan@gmail.com>
2018-10-08 15:57:52 -0700

    added firebase script

---
+++ js/Model/Questions.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCwRRhP3JJZ6yNEZBtba6W-iFkx9lcpGmM",
+    authDomain: "quizcreator-a21b4.firebaseapp.com",
+    databaseURL: "https://quizcreator-a21b4.firebaseio.com",
+    projectId: "quizcreator-a21b4",
+    storageBucket: "",
+    messagingSenderId: "311879722750"
+  };
+  firebase.initializeApp(config);
